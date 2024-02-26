7f848495f9568cbf2d86d3f9d48755c8bb06451a
Anastasiia Dekret <anastasiiadekret@yahoo.com>
2024-02-26 12:20:41 +0100

    removed unnecessary lines

--- src/Weather.js
+++ src/Weather.js
@@ -81,7 +81,7 @@ export default function Weather(props) {
           </a>{" "}
           and{" "}
           <a
-            href=""
+            href="/"
             target="_blank"
             rel="noopener noreferrer"
           >
